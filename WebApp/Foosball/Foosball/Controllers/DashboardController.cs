e0d7901abf110cb3ba1787052e5346499abee4f9
Tim van de Steeg <tim.vandesteeg@altran.com>
2017-09-21 10:37:01 +0200

    Add admin template gentelella to project

--- WebApp/Foosball/Foosball/Controllers/DashboardController.cs
+++ WebApp/Foosball/Foosball/Controllers/DashboardController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Foosball.Models;
 
 namespace Foosball.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly FoosballContext _context;
+
+        public DashboardController(FoosballContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View();            
         }
     }
 }
--- WebApp/Foosball/Foosball/Startup.cs
+++ WebApp/Foosball/Foosball/Startup.cs
@@ -63,7 +63,7 @@ namespace Foosball
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Dashboard}/{action=Index}/{id?}");
             });
         }
     }
