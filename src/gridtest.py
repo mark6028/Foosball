05dc8a3f1c2ea13633c8dd53c6b0b78126600705
mark6028 <mark6028a@gmail.com>
2017-09-22 13:54:50 +0200

    -

--- src/gridtest.py
+++ src/gridtest.py
@@ -1,53 +1,113 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import messagebox
+import time
+import nxppy
+#import RPi.GPIO as GPIO
+#import _thread
 
+
+#mifare = nxppy.Mifare()
+
+def flashbutton(buttonnr,period):
+    while True:
+        print(buttonnr + "Aan")
+        time.sleep(period/2)
+        print(buttonnr + "Uit")
+        time.sleep(period/2)
 
 def defencegrey(*args):
-    try:
-        print("Defence Grey")
-    except:
-        pass
+    #try:
+        win = Toplevel()
+        win.title("scan your tag")
+
+        l = ttk.Label(win, text="Scan your tag", font='helvetica 40')
+        l.grid(row=0, column=0)
+
+        b = ttk.Button(win, text="cancel", command=win.destroy)
+        b.grid(row=1, column=0)
+        #thread.start_new_thread(flashbutton,("GreyDefence",1))
+    #except:
+     #   pass
 
 def attackgrey(*args):
     try:
-        print("Defence Grey")
+        win = Toplevel()
+        win.title("scan your tag")
+        
+        l = ttk.Label(win, text="Scan your tag", font='helvetica 40')
+        l.grid(row=0, column=0)
+
+        b = ttk.Button(win, text="cancel", command=win.destroy)
+        b.grid(row=1, column=0)
     except:
         pass
         
 def defenceblack(*args):
     try:
-        print("Defence Grey")
+        win = Toplevel()
+        win.title("scan your tag")
+        
+        l = ttk.Label(win, text="Scan your tag", font='helvetica 40')
+        l.grid(row=0, column=0)
+
+        b = ttk.Button(win, text="cancel", command=win.destroy)
+        b.grid(row=1, column=0)
     except:
         pass
         
 def attackblack(*args):
     try:
-        print("Defence Grey")
+        win = Toplevel()
+        win.title("scan your tag")
+        
+        l = ttk.Label(win, text="Scan your tag", font='helvetica 40')
+        l.grid(row=0, column=0)
+
+        b = ttk.Button(win, text="cancel", command=win.destroy)
+        b.grid(row=1, column=0)
     except:
         pass
 
+
 root = Tk()
+#root.attributes("-fullscreen", True)
 root.title("Altran Table Football")
+greydefencephoto = PhotoImage(file="../Images/GreyDefence.png")
+greyattackphoto = PhotoImage(file="../Images/GreyAttack.png")
+blackdefencephoto = PhotoImage(file="../Images/BlackDefence.png")
+blackattackphoto = PhotoImage(file="../Images/BlackAttack.png")
+tablephoto = PhotoImage(file="../Images/table.png")
 
 
-content = ttk.Frame(root, padding=(3,3,12,12), width=200, he)
-frame1 = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=240)
-frame2 = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=240)
-frame3 = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=240)
-frame4 = ttk.Frame(content, borderwidth=5, relief="sunken", width=200, height=240)
+content = ttk.Frame(root, padding=(3,3,12,12), width=800, height=480)
+mainframe = ttk.Frame(content, borderwidth=0, relief="sunken", width=800, height=480)
+mainframe.grid_propagate(False)
+frame1 = ttk.Frame(mainframe, borderwidth=0, relief="sunken", width=200, height=240)
+frame1.grid_propagate(False)
+frame2 = ttk.Frame(mainframe, borderwidth=0, relief="sunken", width=200, height=240)
+frame2.grid_propagate(False)
+frame3 = ttk.Frame(mainframe, borderwidth=0, relief="sunken", width=200, height=240)
+frame3.grid_propagate(False)
+frame4 = ttk.Frame(mainframe, borderwidth=0, relief="sunken", width=200, height=240)
+frame4.grid_propagate(False)
+imageframe= ttk.Frame(mainframe, borderwidth=0, relief="sunken", width=400, height=480)
+imageframe.grid_propagate(False)
 
-defencegreybutton = ttk.Button(frame1, text="Defence Grey", command=defencegrey).grid(column=3, row=1, sticky=(W,N))
-attackgreybutton = ttk.Button(frame2, text="Attack Grey", command=defencegrey).grid(column=3, row=3, sticky=(W,S))
-defenceblackbutton = ttk.Button(frame3, text="Defence Black", command=defencegrey).grid(column=1, row=3, sticky=(E,S))
-attackblackbutton = ttk.Button(frame4, text="Attack Black", command=defencegrey).grid(column=1, row=1, sticky=(E,N))
+defencegreybutton = ttk.Button(frame1, image = greydefencephoto, command=defencegrey).grid(sticky=(N, W, S, E))
+attackgreybutton = ttk.Button(frame2, image = greyattackphoto, command=attackgrey).grid(sticky=(N, W, S, E))
+defenceblackbutton = ttk.Button(frame3, image = blackdefencephoto, command=defenceblack).grid(sticky=(N, W, S, E))
+attackblackbutton = ttk.Button(frame4, image = blackattackphoto, command=attackblack).grid(sticky=(N, W, S, E))
+label = ttk.Label(imageframe, image = tablephoto).grid(sticky=(N, W, S, E))
 
 content.grid(column=0, row=0, sticky=(N, S, E, W))
-frame1.grid(column=0, row=0, columnspan=1, rowspan=2, sticky=(N, W))
-frame2.grid(column=0, row=0, columnspan=1, rowspan=2, sticky=(S, W))
-frame3.grid(column=0, row=0, columnspan=1, rowspan=2, sticky=(S, E))
-frame4.grid(column=0, row=0, columnspan=1, rowspan=2, sticky=(N, E))
-#namelbl.grid(column=3, row=0, columnspan=2, sticky=(N, W), padx=5)
-#name.grid(column=3, row=1, columnspan=2, sticky=(N, E, W), pady=5, padx=5)
+mainframe.grid(column=0, row=0, columnspan=1, rowspan=2, sticky=(N, W, S, E))
+frame1.grid(column=3, row=1, columnspan=1, rowspan=1)
+frame2.grid(column=3, row=0, columnspan=1, rowspan=1)
+frame3.grid(column=0, row=0, columnspan=1, rowspan=1)
+frame4.grid(column=0, row=1, columnspan=1, rowspan=1)
+imageframe.grid(column=1,row = 0, columnspan=2, rowspan=2)
+
 
 root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
